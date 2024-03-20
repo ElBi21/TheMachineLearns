c41896a50a7264bcd959d1a9339b1126dbafb308
ElBi21 <64312343+ElBi21@users.noreply.github.com>
2024-03-20 10:17:37 +0100

    Filters

---
+++ AI Lab/05_filters.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+# What is the purpouse of filters in CV? They are an operation which operates on images, such as blurring
+# and so on...
+
+# Convolution is the name of the operation done on the image. There are different types of convolutions:
+#  - 1D convolutions (for signals and so on...);
+#  - 2D convolutions (for images);
+#  - 3D convolutions;
+#  - and so on...
+
+# The convolution is done via a matrix called kernel.
+
