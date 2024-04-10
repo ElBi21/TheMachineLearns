591740138455534e8bd22de3a261bf75cea6ea4a
ElBi21 <64312343+ElBi21@users.noreply.github.com>
2024-04-10 11:14:51 +0200

    Games + Extractin points + NLP

--- AI Lab/10_extracting_points.py
+++ AI Lab/10_extracting_points.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 image = cv2.imread("AI Lab/cat.jpeg")
+image2 = cv2.imread("AI Lab/onboard.png")
 
 # First method: SIFT (Scale Invariant Feature Transform)
 
@@ -22,14 +23,12 @@ keypoints, descriptors = akaze.detectAndCompute(image, None)
 # cv2.drawKeypoints(image, keypoints, image, (0, 255, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # We can try to match two images:
-image2 = cv2.imread("AI Lab/onboard.png")
-
 keypoints2, descriptors2 = akaze.detectAndCompute(image2, None)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-matches = bf.match(descriptors, descriptors2)
+# matches = bf.match(descriptors, descriptors2)
+matches = bf.knnMatch(descriptors, descriptors2, k=2)
 
-image_matches = cv2.drawMatches(image, keypoints, image2, keypoints2, matches, image2, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
+image_matches = cv2.drawMatchesKnn(image, keypoints, image2, keypoints2, matches, image2, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
 cv2.imshow("Image", image_matches)
 cv2.waitKey(0)
---
+++ AI Lab/11_panorama.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+# Load the images
+image1 = cv2.imread("AI Lab/imgs/10_imgs/right.png")
+image2 = cv2.imread("AI Lab/imgs/10_imgs/left.png")
+
+# Create featurer extractor
+orb = cv2.ORB_create()
+
+# Compute features
+kpt1, desc1 = orb.detectAndCompute(image1, None)
+kpt2, desc2 = orb.detectAndCompute(image2, None)
+
+# Create a matcher and match the keypoints
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+matches = matcher.knnMatch(desc1, desc2, k=2)
+
+# Perform the ratio test: a match is said to be correct if the ratio between the two
+# closest points is below a certain threshold
+good_matches = []
+for m, n in matches:
+    if m.distance < 0.3 * n.distance:
+        good_matches.append(m)
+
+# Check if at least 4 points have been selected
+# Remember that the two images are called queryImg and TrainingImg, so queryIdx is a point
+# belonging to queryImg, while TrainingIdx is a point belonging to TrainingImg
+if len(good_matches) > 4:
+    # Convert to float32
+    src_points = np.float32([kpt1[m.queryIdx].pt for m in good_matches])
+    dst_points = np.float32([kpt2[m.trainIdx].pt for m in good_matches])
+
+    # Compute the homography matrix
+    M, mask = cv2.findHomography(src_points, dst_points)
+
+    # Transform left image and stitch it together with the right image
+    dst = cv2.warpPerspective(image1, M, (image1.shape[1] + image2.shape[1], image1.shape[0]))
+
+    dst[0:image2.shape[0], 0:image2.shape[1]] = image2.copy()
+
+    cv2.namedWindow("Panorama", cv2.WINDOW_KEEPRATIO)
+    cv2.imshow("Panorama", dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
